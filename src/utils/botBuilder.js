c2046633bb119ec5eb55565ed1542180fd670041
Akarsh <akarsh.prasad@lotusdew.in>
2020-09-03 20:00:19 +0530

    code refactor

--- src/utils/botBuilder.js
+++ src/utils/botBuilder.js
@@ -9,14 +9,15 @@ export const reorderNodes = (data) => {
     let visited = [defaultNode];
     orderedList.push(firstNodeObj);
     while (options.length > 0) {
-            if(visited.indexOf(options[0]) === -1) {
-                visited.push(options[0]);
-                let newNode = data[options[0]];
-                orderedList.push(newNode);
-                currentOptions = (newNode.options || []).map(z => z.optionHtml ? z.optionHtml.nextId : z.id);
-                options = [...options, ...currentOptions];
-            }
-            options.shift();
+        const firstOption = options[0];
+        if(visited.indexOf(firstOption) === -1) {
+            visited.push(firstOption);
+            let newNode = data[firstOption];
+            orderedList.push(newNode);
+            currentOptions = (newNode.options || []).map(z => z.optionHtml ? z.optionHtml.nextId : z.id);
+            options = [...options, ...currentOptions];
+        }
+        options.shift();
     }
     return orderedList;
 }
